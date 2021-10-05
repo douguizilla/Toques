bb5795508190758eec8ed791808da83f288bf6ae
douguizilla <douglasgommez@gmail.com>
2021-10-05 18:12:24 -0300

    implemented TouchActivity.kt

--- app/src/main/java/com/odougle/toques/TouchActivity.kt
+++ app/src/main/java/com/odougle/toques/TouchActivity.kt
@@ -2,10 +2,52 @@ package com.odougle.toques
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.TextView
 
 class TouchActivity : AppCompatActivity() {
+    private lateinit var txtTouchEvent: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_touch)
+        txtTouchEvent = TextView(this)
+        setContentView(txtTouchEvent)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return super.onTouchEvent(event)
+
+        var log = "onTouch\n"
+
+        val consumed = when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                log += "ACTION_DOWN\n"
+                true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                log += "ACTION_MOVE\n"
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                log += "ACTION_UP\n"
+                true
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                log += "ACTION_CANCEL\n"
+                true
+            }
+            MotionEvent.ACTION_OUTSIDE -> {
+                log += "ACTION_OUTSIDE\n"
+                true
+            }
+            else -> true
+        }
+        val fingerOnScreen: Int = event.pointerCount
+        log += "Dedos na tela: $fingerOnScreen\n"
+        for (i in 0 until fingerOnScreen) {
+            log += "$i x=${event.getX(i)}, y=${event.getY(i)}\n"
+        }
+        txtTouchEvent.text = log
+        return consumed
     }
 }
